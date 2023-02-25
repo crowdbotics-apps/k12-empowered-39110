55b41748e4b0b59180fedd232b4f5d11374dfc94
Crowdbotics <team@crowdbotics.com>
2023-02-25 13:46:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "k12-empowered-39110",
+  url: "https://k12-empowered-39110.botics.co",
+  api: "https://k12-empowered-39110.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
